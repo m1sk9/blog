0f7395a610f107b12ff28f86255ecb8dd207bd8e
Sho Sakuma <me@m1s.dev>
2024-11-07 00:38:38 +0900

    fix: dont working toggle theme logic

--- src/utils/toggle-theme.ts
+++ src/utils/toggle-theme.ts
@@ -1,13 +1,11 @@
-const primaryColorScheme = 'light';
+const primaryColorScheme = '';
 const currentTheme = localStorage.getItem('theme');
 
 function getPreferTheme() {
   if (currentTheme) return currentTheme;
 
-  // return primary color scheme if it is set
   if (primaryColorScheme) return primaryColorScheme;
 
-  // return user device's prefer color scheme
   return window.matchMedia('(prefers-color-scheme: dark)').matches
     ? 'dark'
     : 'light';
@@ -26,32 +24,22 @@ function reflectPreference() {
 
   document.querySelector('#theme-btn')?.setAttribute('aria-label', themeValue);
 
-  // Get a reference to the body element
   const body = document.body;
 
   if (body) {
-    // Get the computed styles for the body element
     const computedStyles = window.getComputedStyle(body);
-
-    // Get the background color property
     const bgColor = computedStyles.backgroundColor;
 
-    // Set the background color in <meta theme-color ... />
     document
       .querySelector("meta[name='theme-color']")
       ?.setAttribute('content', bgColor);
   }
 }
 
-// set early so no page flashes / CSS is made aware
 reflectPreference();
-
 window.onload = () => {
   function setThemeFeature() {
-    // set on load so screen readers can get the latest value on the button
     reflectPreference();
-
-    // now this script can find and listen for clicks on the control
     document.querySelector('#theme-btn')?.addEventListener('click', () => {
       themeValue = themeValue === 'light' ? 'dark' : 'light';
       setPreference();
@@ -60,11 +48,9 @@ window.onload = () => {
 
   setThemeFeature();
 
-  // Runs on view transitions navigation
   document.addEventListener('astro:after-swap', setThemeFeature);
 };
 
-// sync with system changes
 window
   .matchMedia('(prefers-color-scheme: dark)')
   .addEventListener('change', ({ matches: isDark }) => {
